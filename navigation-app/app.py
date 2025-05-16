3a3aa961b0329256afa3b46863e7c9c914a639a3
jdsi23 <jameslusk535@gmail.com>
2025-05-16 17:06:04 -0400

    Update app.py

--- navigation-app/app.py
+++ navigation-app/app.py
@@ -9,59 +9,25 @@ def home():
     <html>
     <head>
         <title>Mystery Island Navigation</title>
-        <style>
-            .map-container {
-                position: relative;
-                display: inline-block;
-            }
-            .icon {
-                position: absolute;
-                width: 40px;
-                cursor: pointer;
-                transition: transform 0.2s ease;
-            }
-            .icon:hover {
-                transform: scale(1.3);
-                z-index: 2;
-             }
-             #directions {
-                margin-top: 20px;
-                font-family: sans-serif;
-                padding: 10px;
-                border: 1px solid #aaa;
-                width: 60%;
-                background: #f7f7f7;
-            }
-        </style>
-        <script>
-            async function getDirections(destination) {
-                const res = await fetch(`/directions?to=${destination}`);
-                const data = await res.json();
-                let html = `<h3>Route to ${data.destination}</h3><ol>`;
-                data.route.forEach(step => {
-                    html += `<li>${step}</li>`;
-                });
-                html += `</ol>`;
-                document.getElementById("directions").innerHTML = html;
-            }
-        </script>
+        <link rel="stylesheet" href="styles.css">
+        <script src="script.js" defer></script>
     </head>
-    <body style='text-align:center;'>
+    <body style="text-align:center;">
         <h1>🗺️ Welcome to Mystery Island</h1>
         <div class="map-container">
-            <img src='https://i.postimg.cc/7hTkGYNG/map.png' alt='Mystery Island Map' style='width:90%;'>
-            <img src='https://i.postimg.cc/HswLBBQJ/icon-volcano.png' class='icon'
-                title="🔥 Ashen Secrets 🔥"
-                style='top:70px; left:120px;' onclick="getDirections('volcano')">
-            <img src='https://i.postimg.cc/nr8H1DrY/icon-maze.png' class='icon'
-                title="🌿 Maze of Whisper 🌿"
-                style='top:220px; left:60px;' onclick="getDirections('maze')">
-           <img src='https://i.postimg.cc/J7B0B105/icon-boat.png' class='icon'
-                title="🚤 The Forgotten Current 🚤"
-                style='top:400px; left:220px;' onclick="getDirections('boat')">
-            <img src='https://i.postimg.cc/QdNjSs8P/icon-resort.png' class='icon'
-                title="🌺 Secrets of the Sands 🌺"
-                style='top:100px; left:320px;' onclick="getDirections('resort')">
+            <img src="https://i.postimg.cc/7hTkGYNG/map.png" alt="Mystery Island Map" style="width:90%;">
+            <img src="https://i.postimg.cc/HswLBBQJ/icon-volcano.png" class="icon"
+                 title="🔥 Ashen Secrets 🔥"
+                 style="top: 100px; left:150px; width: 85px;" onclick="getDirections('volcano')">
+            <img src="https://i.postimg.cc/nr8H1DrY/icon-maze.png" class="icon"
+                 title="🌿 Maze of Whisper 🌿"
+                 style="top:410px; left:135px; width: 85px;" onclick="getDirections('maze')">
+            <img src="https://i.postimg.cc/J7B0B105/icon-boat.png" class="icon"
+                 title="🚤 The Forgotten Current 🚤"
+                 style="top:450px; left:600px; width: 65px;" onclick="getDirections('boat')">
+            <img src="https://i.postimg.cc/QdNjSs8P/icon-resort.png" class="icon"
+                 title="🌺 Secrets of the Sands 🌺"
+                 style="top:80px; left:600px; width: 85px;" onclick="getDirections('resort')">
         </div>
         <div id="directions"></div>
     </body>
